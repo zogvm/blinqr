825d43f06b598e62f72f669b1a3e76cc38e19648
zogvm <zogna@qq.com>
2022-01-05 14:08:17 +0800

    多进程
    多进程

--- blinqrmt.py
+++ blinqrmt.py
@@ -13,8 +13,10 @@ from termcolor import colored
 from lt import encode, decode
 from base64 import b85encode, b85decode
 from tkinter.filedialog import askopenfilename
-import queue
-import threading
+
+import multiprocessing
+
+isDebug=False
 
 def fix_scaling():
     import sys
@@ -41,34 +43,42 @@ def calculate_sha1(data: bytes) -> str:
     return m.hexdigest()
 
 def set_block(stream,block_size,q_block):
+    i=0     
+    start_time = time.time()
     for block in encode.encoder(stream, block_size):  # max size is 2350
         q_block.put(block,True)
-        #time.sleep(0.1)	
+        #time.sleep(0.01)	
         
-def set_img(q_block,q_img):
+        if isDebug:
+            end_time = time.time()
+            i+=1
+            print("block time:{:.2f},{:d}".format(end_time-start_time,i))
+            start_time = time.time()
+ 
+img_i=0     
+def set_img(index,q_block,q_img):
+    global img_i
     while True:
-        if q_block.qsize()>0:		
+        if q_block.qsize()>0:	
+            if isDebug:	
+                start_time = time.time()	
+            #make_qr最耗 差不多0.2S
             block=q_block.get()
             block_encoded = b85encode(block)
             qr = make_qr(block_encoded, error='l')
-        
             img = ~np.array(qr.matrix, dtype=np.bool)
             img = np.uint8(img) * 0xFF
             img = np.pad(img, pad_width=6, mode='constant', constant_values=0xFF)
             q_img.put(img,True)
-        else:
-            time.sleep(0.05)	
         
-    
-def get_img(q_img):
-    while True:
-        if q_img.qsize()>0:	
-            img=q_img.get()
-            cv2.imshow('sender', img)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            if isDebug:
+                end_time = time.time()
+                img_i=img_i+1
+                print("index={:d},img time:{:.2f},{:d}".format(index,end_time-start_time,img_i))
+           
         else:
-            time.sleep(0.05)	
+            time.sleep(0.01)	
+        
 
 
 def send(data: bytes, *, block_size: int = 2350):
@@ -76,28 +86,32 @@ def send(data: bytes, *, block_size: int = 2350):
 
     sha1 = calculate_sha1(data)
     stream = BytesIO(data)
-    print(colored(f'{len(data)} bytes, SHA-1: {sha1}', 'blue'))
+    print(colored(f'多进程模式{len(data)} bytes, SHA-1: {sha1}', 'blue'))
 
     cv2.namedWindow('sender', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('sender', 960, 960)
     
-    q_block = queue.Queue(5)
-    q_img = queue.Queue(5)
-    t1 = threading.Thread(target=set_block,args=[stream,block_size,q_block])
-    t2 = threading.Thread(target=set_img,args=[q_block,q_img])
-    t3 = threading.Thread(target=set_img,args=[q_block,q_img])
+    start_time = time.time()
+    i=0     
+    q_block = multiprocessing.Queue(30)
+    q_img = multiprocessing.Queue(30)
+    t1 = multiprocessing.Process(target=set_block,args=[stream,block_size,q_block])
     t1.start()
-    t2.start()
-    t3.start()
+    #设为5差不多为15FPS
+    for x in range(5):
+        t2 = multiprocessing.Process(target=set_img,args=[x,q_block,q_img])
+        t2.start()
     
     while True:
         if q_img.qsize()>0:	
+            i+=1
+            print("time:{:.2f},{:d}".format(time.time()-start_time,i))
             img=q_img.get()
             cv2.imshow('sender', img)
             if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+               break
         else:
-            time.sleep(0.1)	
+            time.sleep(0.05)	
            
     cv2.destroyAllWindows()   
     
---
+++ sender_run.bat
@@ -0,0 +1,2 @@
+python sender.py
+pause
