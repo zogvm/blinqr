3b7b517da55109d6f40fd1554b4744b126d0a8d2
zogvm <zogna@qq.com>
2022-01-06 12:11:57 +0800

    多进程接收10KB/S！

--- blinqrmt.py
+++ blinqrmt.py
@@ -42,20 +42,29 @@ def calculate_sha1(data: bytes) -> str:
     m.update(data)
     return m.hexdigest()
 
+#========================send==========start========================
+
+isEnd=False
 def set_block(stream,block_size,q_block):
+    global isEnd   
     i=0     
     start_time = time.time()
     for block in encode.encoder(stream, block_size):  # max size is 2350
         q_block.put(block,True)
-        #time.sleep(0.01)	
-        
+        #time.sleep(0.001)	
+        if isEnd:
+            break
+
         if isDebug:
             end_time = time.time()
             i+=1
             print("block time:{:.2f},{:d}".format(end_time-start_time,i))
             start_time = time.time()
- 
+        
+    isEnd=True
+
 def set_img(index,q_block,q_img):
+    global isEnd   
     while True:
         if q_block.qsize()>0:	
             if isDebug:	
@@ -77,13 +86,14 @@ def set_img(index,q_block,q_img):
                 print("index={:d},img time:{:.2f}".format(index,end_time-start_time))
            
         else:
-            time.sleep(0.01)	
+            time.sleep(0.001)	
+            if isEnd:
+                break
         
-
-
 def send(data: bytes, *, block_size: int = 2350):
-    #assert block_size <= 2350
+    assert block_size <= 2350
 
+    global isEnd  
     sha1 = calculate_sha1(data)
     stream = BytesIO(data)
     print(colored(f'多进程模式{len(data)} bytes, SHA-1: {sha1}', 'blue'))
@@ -136,6 +146,9 @@ def send(data: bytes, *, block_size: int = 2350):
         t2.start()
     
     while True:
+        if isEnd and q_block.qsize()==0 and q_img.qsize()==0:
+            print("send end")
+            break
         if q_img.qsize()>0:	
             i+=1
             print("time:{:.2f},{:d}".format(time.time()-start_time,i))
@@ -155,44 +168,63 @@ def send(data: bytes, *, block_size: int = 2350):
         #不要太快 会糊
         time.sleep(0.2)	
            
-    cv2.destroyAllWindows()   
-    
-        
+    cv2.destroyAllWindows()  
+    isEnd=True
 
-def receive(path):
-    qrDecoder = cv2.QRCodeDetector()
-    ltDecoder = decode.LtDecoder()
-    empty = True
+ #====================send=========end=================   
+
+ #====================receive======start================    
+def read_cap(path,q_img):
+    global isEnd 
     i=0
-    j=0
+    start_time = time.time()
+
     if not path:
         cap = cv2.VideoCapture(0)
     else:
         cap = cv2.VideoCapture(path)
-        
+    #这里path也可以填rtsp
+
     w=cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     h=cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     print("w:{:f},h:{:f}".format(w,h))
-    
-    try:
-        while True:
-            ret, img = cap.read()
-            if not ret:
-                break
-                
-            #time.sleep(1)    
-            
-            start_f_time = time.time()
-            if empty:
+
+    while True:
+        ret, img = cap.read()
+        if not ret:
+           break
+        q_img.put(img,True)
+        #time.sleep(0.03)  #33fps for realtime 
+        time.sleep(0.001)
+        if isEnd:
+            break
+
+        i+=1
+        print("f time:{:.2f},{:d}".format(time.time()-start_time,i))
+       
+
+    cap.release()       
+    isEnd=True
+
+def decoded_img(index,q_img,q_decode):
+    global isEnd 
+    while True:
+        if isEnd:
+            break  
+        time.sleep(0.001)	
+        if q_img.qsize()>0:	
+
+            if isDebug:
                 start_time = time.time()
-            
+
+            img=q_img.get()
             decoded_qrs = pyzbar.decode(img, symbols=(pyzbar.ZBarSymbol.QRCODE,))
-           
+            
             #灰色    
             if not decoded_qrs:
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                 decoded_qrs = pyzbar.decode(img, symbols=(pyzbar.ZBarSymbol.QRCODE,))
-            
+                
             #锐化
             if False:
                 if not decoded_qrs:
@@ -200,7 +232,7 @@ def receive(path):
                     k= np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
                     img=cv2.filter2D(img_src,-1,k)
                     decoded_qrs = pyzbar.decode(img, symbols=(pyzbar.ZBarSymbol.QRCODE,))
-                
+                    
             #gamma校准
             if False:
                 img_src=img	
@@ -213,8 +245,8 @@ def receive(path):
                     decoded_qrs = pyzbar.decode(img, symbols=(pyzbar.ZBarSymbol.QRCODE,))
                     thre=thre+0.1
                 img=img_src
+                    
                 
-            
             #缩小后边缘扩大
             if False:
                 rate=0.9
@@ -224,8 +256,8 @@ def receive(path):
                     new_h=int(h-h*rate)
                     img = cv2.copyMakeBorder(img,new_h,new_h,new_w,new_w, cv2.BORDER_CONSTANT,value=0xFF)
                     decoded_qrs = pyzbar.decode(img, symbols=(pyzbar.ZBarSymbol.QRCODE,))
-            
                 
+                    
             #二值化 只开这个效果好
             if True:
                 img_src=img
@@ -235,50 +267,89 @@ def receive(path):
                     decoded_qrs = pyzbar.decode(img, symbols=(pyzbar.ZBarSymbol.QRCODE,))
                     thre = thre + 10
                 img=img_src
-                
+            
             if decoded_qrs:
-                step_qrs_time = time.time()
                 for decoded_qr in decoded_qrs:
-                    polygon = tuple(decoded_qr.polygon)
-                    for p1, p2 in zip(polygon, (*polygon[1:], polygon[0])):
-                        cv2.line(img, (p1.x, p1.y), (p2.x, p2.y), (255, 0, 0), 3)
-
-                    data_encoded = decoded_qr.data
-                    data_decoded = b85decode(data_encoded)
-
-                    block = decode.block_from_bytes(data_decoded)
-                    ltDecoder.consume_block(block)
-                    empty = False
+                    q_decode.put(decoded_qr,True)
                     
-                    if ltDecoder.is_done():
-                        data = ltDecoder.bytes_dump()
-                        end_time = time.time()
-                        sha1 = calculate_sha1(data)
-                        time_elapsed = end_time - start_time
-                        transfer_speed = len(data) / time_elapsed
+            if isDebug:
+                print("index={:d},decode time:{:.2f}".format(index,time.time()-start_time))
 
-                        print(colored(f'done:{len(data)} bytes, SHA-1: {sha1}', 'green'))
-                        print(colored(f'done:{time_elapsed:.2f} seconds, {transfer_speed:.2f} B/s', 'green'))
-                        time.sleep(3)
-                        
-                        with open(path+".out", 'wb') as fw:
-                            data = fw.write(data)
-                        exit(0)
+      
+            
 
-                        ltDecoder = decode.LtDecoder()
-                        empty = True
+           
+def read_decode(index,q_decode,q_block):
+    global isEnd 
+    while True:
+        if isEnd:
+            break   
+        time.sleep(0.001)	
+        if q_decode.qsize()>0:	
+            if isDebug:
+                start_time = time.time()
 
-                    else:
-                        j+=1
-                        print("qrs time:{:.2f},{:d}".format(time.time()-step_qrs_time,j))
-            else:
-                i+=1
-                print("f time:{:.2f},{:d}".format(time.time()-start_f_time,i))
+            decoded_qr=q_decode.get()
 
-            cv2.imshow('receiver', img)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            data_decoded = b85decode(decoded_qr.data)
+            block = decode.block_from_bytes(data_decoded)
+            q_block.put(block,True)
+
+            if isDebug:
+                print("index={:d},block time:{:.2f}".format(index,time.time()-start_time))
+
+     
+
+def receive(path):
+    global isEnd       
+    ltDecoder = decode.LtDecoder()
+    empty = True
+
+    q_img = multiprocessing.Queue(30)
+    q_decode = multiprocessing.Queue(30)
+    q_block = multiprocessing.Queue(30)
+    t1 = multiprocessing.Process(target=read_cap,args=[path,q_img])
+    t1.start()
+    #
+    for x in range(5):
+        t2 = multiprocessing.Process(target=decoded_img,args=[x,q_img,q_decode])
+        t2.start()
+
+    for x in range(1):
+        t3 = multiprocessing.Process(target=read_decode,args=[x,q_decode,q_block])
+        t3.start()
+
+    start_time = time.time()
+    try:
+        while True:
+            if isEnd and q_decode.qsize()==0 and q_block.qsize()==0 and q_img.qsize()==0:
+                print("receive end")
                 break
+            if q_block.qsize()>0:	
+                block=q_block.get()
+                ltDecoder.consume_block(block)
+                empty = False
 
+                if ltDecoder.is_done():
+                    data = ltDecoder.bytes_dump()
+                    sha1 = calculate_sha1(data)
+
+                    time_elapsed = time.time() - start_time
+                    transfer_speed = len(data) / time_elapsed
+                    print(colored(f'done:{len(data)} bytes, SHA-1: {sha1}', 'green'))
+                    print(colored(f'done:{time_elapsed:.2f} seconds, {transfer_speed:.2f} B/s', 'green'))
+                 
+                    with open(path+".out", 'wb') as fw:
+                        data = fw.write(data)
+                    break
+
+                    ltDecoder = decode.LtDecoder()
+                    empty = True
+            else:
+                time.sleep(0.001)
     finally:
-        cap.release()
         cv2.destroyAllWindows()
+ 
+    isEnd=True
+
+ #====================receive======end================    
